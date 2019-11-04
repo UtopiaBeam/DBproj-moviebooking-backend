e10599d54149e90bb9b180d08582dd6ca86d7bed
UtopiaBeam <utopiabeam@gmail.com>
2019-11-04 10:02:22 +0700

    :truck: Flatten directory

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Params } from 'express-serve-static-core';
 
 @Controller()
 export class AppController {
