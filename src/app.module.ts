cfedac285a0ed24a8e78f3d9de9a79e6a2a57bac
sprkzoff <sprkzoff@gmail.com>
2019-11-19 10:07:00 +0700

    :zap: update app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { MovieService } from './movie/movie.service';
+import { MovieController } from './movie/movie.controller';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [UserModule, MovieModule],
+    controllers: [AppController, UserController, MovieController],
+    providers: [AppService, UserService, MovieService],
 })
 export class AppModule {}
