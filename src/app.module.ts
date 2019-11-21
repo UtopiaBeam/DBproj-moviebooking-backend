f21b2d969ec580a87afd3fc17630fa8c9f68ac80
UtopiaBeam <utopiabeam@gmail.com>
2019-11-21 13:42:37 +0700

    :card_file_box: Connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,30 @@ import { UserModule } from './user/user.module';
 import { MovieService } from './movie/movie.service';
 import { MovieController } from './movie/movie.controller';
 import { MovieModule } from './movie/movie.module';
+import { ConfigModule } from './config/config.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from './config/config.service';
 
 @Module({
-    imports: [UserModule, MovieModule],
+    imports: [
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                type: 'mysql',
+                host: config.mySqlUrl,
+                port: 3306,
+                username: config.mySqlUser,
+                password: config.mySqlPassword,
+                database: config.mySqlDatabase,
+                entities: [__dirname + '/**/*.entity.{js,ts}'],
+                synchronize: true,
+            }),
+        }),
+        UserModule,
+        MovieModule,
+        ConfigModule,
+    ],
     controllers: [AppController, UserController, MovieController],
     providers: [AppService, UserService, MovieService],
 })
