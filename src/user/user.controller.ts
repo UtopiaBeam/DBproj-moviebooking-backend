63ef0bd2874b3bcb7773b7363d558a643e32d172
UtopiaBeam <utopiabeam@gmail.com>
2019-11-21 23:09:58 +0700

    :sparkles: Add raw SQL query example

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from './user.entity';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
     @Get()
-    findAll(): string {
+    findAll(): Promise<User[]> {
         return this.userService.findAll();
     }
 }
