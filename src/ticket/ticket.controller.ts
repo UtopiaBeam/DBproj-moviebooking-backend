0de4e0868d8f0153dba99483e7aa886b5fef594f
sprkzoff <sprkzoff@gmail.com>
2019-11-23 21:54:36 +0700

    ticket service

---
+++ src/ticket/ticket.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Param, Post, Req, Res, Delete} from '@nestjs/common';
+import { TicketService } from './ticket.service';
+import { Ticket } from './ticket.entity';
+
+@Controller('ticket')
+export class TicketController {
+    constructor(private readonly ticketService: TicketService) {}
+
+    @Get()
+    findAll(): Promise<Ticket[]> {
+        return this.ticketService.findAll();
+    }
+
+    @Get(':id')
+    findOnebyid( @Param() params): Promise<Ticket> {
+        return this.ticketService.findOnebyid(params.showTime_id,params.seat_id,params.user_id);
+    }
+
+    @Post('create')
+    createTicket(@Req() req, @Res() res) {
+        return this.ticketService.createTicket(req,res);
+    }
+
+
+    @Delete('delete/:id')
+    deleteTicket(@Param() params) : string {
+        return this.ticketService.deleteTicket(params.id);
+    }
+}
---
+++ src/ticket/ticket.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
+import { Ticket } from './ticket.entity';
+
+@Injectable()
+export class TicketService {
+    constructor(
+        @InjectEntityManager() private readonly manager: EntityManager,
+    ) {}
+
+    findAll(): Promise<Ticket[]> {
+        console.log('all tickets');
+        return this.manager.query(`SELECT * FROM ticket`);
+    }
+
+    findOnebyid(showTime_id,seat_id,user_id) : Promise<Ticket> {
+        console.log('one ticket');
+        return this.manager.query(`SELECT * FROM ticket WHERE showTime_id = ${showTime_id} AND seat_id = ${seat_id} AND user_id = ${user_id}`);
+    }
+
+    createTicket(req,res) {
+        res = this.manager.query(`INSERT INTO ticket (showTime_id,seat_id,user_id,ticket_ref) VALUES (${req.showTime_id},${req.seat_id},${req.user_id},${req.ticket_ref})`);
+        return res.json();
+    }
+
+    updateTicket(req,res) {
+        res = this.manager.query(`UPDATE ticket SET user_id = ${req.user_id}, showTime_id = ${req.showTime_id}, seat_id = ${req.seat_id}, ticket_ref = ${req.ticket_ref}`);
+        return res.json();
+    }
+
+    
+}
+deleteTicket(showTime_id,seat_id,user_id): string {
+        var res = this.manager.query(`DELETE FROM ticket WHERE showTime_id = ${showTime_id} AND seat_id = ${seat_id} AND user_id = ${user_id}`);
+        return `Ticket deleted`;
+    }
+}
